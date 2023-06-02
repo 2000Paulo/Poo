8004352760905d76a97c2c09712db5ca1553f623
Paulo victor melo <paaulomsf@gmail.com>
2023-06-02 00:06:39 -0300

    Herança

---
+++ Ex_Herança.py
@@ -0,0 +1,48 @@
+class Planeta:
+    def __init__(self, nome, diametro, populacao):
+        self.nome = nome
+        self.diametro = diametro
+        self.populacao = populacao
+    
+    def apresentar(self):
+        print(f"Planeta: {self.nome}")
+        print(f"Diametro: {self.diametro}")
+        print(f"População: {self.populacao}")
+
+
+class PlanetaTerra(Planeta):
+    def __init__(self, nome, diametro, populacao, continentes):
+        super().__init__(nome, diametro, populacao)
+        self.continentes = continentes
+    
+    def apresentar(self):
+        super().apresentar()
+        print(f"Continentes: {self.continentes}")
+
+
+class PlanetaMarte(Planeta):
+    def __init__(self, nome, diametro, populacao, exploracoes):
+        super().__init__(nome, diametro, populacao)
+        self.exploracoes = exploracoes
+    
+    def apresentar(self):
+        super().apresentar()
+        print(f"Explorações: {self.exploracoes}")
+
+
+planeta1 = Planeta("Júpiter", 139820, 0)
+terra1 = PlanetaTerra("Terra", 12742, 7700000000, 7)
+marte1 = PlanetaMarte("Marte", 6779, 0, "Curiosity, Perseverance")
+
+
+
+print("Planeta 1:")
+planeta1.apresentar()
+print()
+
+print("Terra 1:")
+terra1.apresentar()
+print()
+
+print("Marte 1:")
+marte1.apresentar()
